f75a85ec854e5fb33cc7d8be94dc63e39c443d88
Yash2204V <yash.v220104@gmail.com>
2023-11-04 17:28:21 +0530

    Initial commit

---
+++ 1_firstProgram.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "Namaste Duniya" << endl;
+/*     cout<< "Namaste  Duniya" << endl;
+    cout<< "Hello World" << endl << endl;
+
+    int a = 23;
+    cout << a << endl;
+    char b = 'y';
+    cout << b << endl;
+    float c = 1.3;
+    cout << c << endl;
+    double d = 1.234;
+    cout << d << endl;
+
+    int size = sizeof(d);
+    cout << "Size of d: "<< size << endl;
+
+    bool e = true;
+    cout << "It's true: "<< e << endl;
+    unsigned int l = 34;
+    cout << l << endl;
+    */
+
+    int a = 3, b = 4;
+    bool first = (a == b);
+    cout << first << endl;
+
+    bool second = (a<b);
+    cout << second << endl;
+
+    bool third = (a>b);
+    cout << third << endl;
+
+    bool fourth = (a>=b);
+    cout << fourth << endl;
+
+    bool fifth = (a>=b);
+    cout << fifth << endl;
+
+    if(a == 3 && b == 4){
+        cout << "Condition is true";
+    }
+    else{
+        cout << "Condition is not true";
+    }
+    cout << '\n' << '\n';
+    double m = 2.478473/2;
+    cout << m << endl;
+
+}
